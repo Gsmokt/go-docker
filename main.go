d67efe282f9998cbcae3e7c516ebb4cd0eb8f749
Grzegorz Smoktunowicz <biarth@wp.pl>
2024-04-19 23:54:34 +0200

    Add htmx - change gin for labstack

--- main.go
+++ main.go
@@ -2,23 +2,80 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
-	router := gin.Default()
-	router.GET("/home", testRoute)
+type Templates struct {
+	templates *template.Template
+}
 
-	router.Run(":8080")
+type Count struct {
+	Count int
+}
+
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("john", "john@com.pl"),
+			newContact("anna", "anna@com.pl"),
+		},
+	}
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	data := newData()
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", data)
+	})
+
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+		data.Contacts = append(data.Contacts, newContact(name, email))
+		return c.Render(200, "display", data)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func welcome(s string) string {
 	return fmt.Sprintf("Welcome, %s", s)
-}
-
-func testRoute(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "welcome",
-	})
 }
